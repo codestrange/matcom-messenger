c72bdb05fa322391f88a9d7dcd6af239f4f943f7
leynier <leynier41@gmail.com>
2019-11-02 17:41:31 -0400

    Modify BuckerTable class

--- kademlia/bucket_table.py
+++ kademlia/bucket_table.py
@@ -11,14 +11,11 @@ class BucketTable:
         for _ in range(b):
             self.buckets.append(Bucket(self.k))
 
-    def get_bucket(self, contact:Contact) -> Bucket:
-        distance = self.hash ^ contact.hash
+    def get_bucket(self, hash:int) -> Bucket:
+        distance = self.hash ^ hash
         index = max([i for i in range(self.b) if (distance & (1<<i)) > 0])
         return self.buckets[index]
-        
 
     def update(self, contact:Contact) -> bool:
-        bucket = self.get_bucket(contact)
+        bucket = self.get_bucket(contact.hash)
         return bucket.update(contact)
-
-        
