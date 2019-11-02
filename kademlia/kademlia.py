f844dc37514d2135568d85d1eba6e8d240e2c715
leynier <leynier41@gmail.com>
2019-11-02 18:59:47 -0400

    Implement base for KademliaService

---
+++ kademlia/kademlia.py
@@ -0,0 +1,23 @@
+from rpyc import Connection
+from contact import 
+from protocol import ProtocolService
+
+
+class KademliaService(ProtocolService):
+    def __init__(self, id:int, k: int, b:int, a:int)
+        super(KademliaService, self).__init__(id, k, b)
+
+    def on_connect(self, conn:Connection):
+        pass
+
+    def on_disconnect(self, conn:Connection):
+        pass
+
+    def exposed_client_store(self, key:int, value:object) -> bool:
+        pass
+
+    def exposed_client_find_node(self, id:int) -> list:
+        pass
+
+    def exposed_client_find_value(self, key:int) -> object:
+        pass
