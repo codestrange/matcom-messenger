739397b5cf5cc9b7c185020c1c9a55d5724c64bd
leynier <leynier41@gmail.com>
2019-11-02 14:54:03 -0400

    Add build in equal method to Contact class

--- kademlia/contact.py
+++ kademlia/contact.py
@@ -1,5 +1,8 @@
 class Contact:
-    def __init__(self, hash:str=None, ip:str=None, port:int=None)
+    def __init__(self, hash:str=None, ip:str=None, port:int=None):
         self.hash = hash
         self.ip = ip
         self.port = port
+
+    def __eq__(self, contact:Contact) -> bool:
+        return self.hash = contact.hash and self.ip = contact.ip and self.port = contact.port
