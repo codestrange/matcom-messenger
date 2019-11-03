bcffcf1071b06d55fbb4bce88facc804d7cf3da0
stdevCbermudez <cbermu.97@gmail.com>
2019-11-03 18:06:12 -0500

    Add Lamport's Clocks logic to ProtocolService class

--- kademlia/protocol.py
+++ kademlia/protocol.py
@@ -9,6 +9,7 @@ class ProtocolService(Service):
     def __init__(self, my_contact:Contact, k:int, b:int, value_cloner):
         super(ProtocolService, self).__init__()
         self.data = {}
+        self.lamport = 0
         self.my_contact = Contact.clone(my_contact)
         self.table = BucketTable(k, b, my_contact.hash)
         self.value_cloner = value_cloner
@@ -19,25 +20,29 @@ class ProtocolService(Service):
     def on_disconnect(self, conn:Connection):
         pass
 
-    def exposed_store(self, client:Contact, key:int, value:object) -> bool:
+    def exposed_store(self, client:Contact, client_lamport:int, key:int, value:object) -> bool:
         client = Contact.clone(client)
         value = self.value_cloner(value)
+        self.lamport = max(client_lamport, self.lamport + 1) 
         self.update_contact(client)
         self.data[key] = value
         return True
 
-    def exposed_ping(self, client:Contact) -> bool:
+    def exposed_ping(self, client:Contact, client_lamport:int) -> bool:
         client = Contact.clone(client)
+        self.lamport = max(client_lamport, self.lamport + 1)
         self.update_contact(client)
         return True
 
-    def exposed_find_node(self, client:Contact, id:int) -> list:
+    def exposed_find_node(self, client:Contact, client_lamport:int, id:int) -> list:
         client = Contact.clone(client)
+        self.lamport = max(client_lamport, self.lamport + 1)
         self.update_contact(client)
         return self.table.get_bucket(id).nodes
 
-    def exposed_find_value(self, client:Contact, key:int) -> object:
+    def exposed_find_value(self, client:Contact, client_lamport:int, key:int) -> object:
         client = Contact.clone(client)
+        self.lamport = max(client_lamport, self.lamport + 1)
         self.update_contact(client)
         try:
             return self.data[key]
@@ -61,5 +66,6 @@ class ProtocolService(Service):
     def ping(self, ip, port):
         connection = connect(ip, str(port))
         connection.ping()
-        connection.root.ping()
+        self.lamport += 1
+        connection.root.ping(self.my_contact, self.lamport)
         return connection
