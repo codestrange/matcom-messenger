8c893aef8efc9e3e514e60b25533a23fd77173bf
stdevCbermudez <cbermu.97@gmail.com>
2019-11-03 17:05:26 -0500

    Implement KademliaService client_find_node method

--- kademlia/kademlia.py
+++ kademlia/kademlia.py
@@ -1,11 +1,15 @@
+from queue import Empty, Queue
+from threading import Semaphore
 from rpyc import Connection
 from .contact import Contact
 from .protocol import ProtocolService
+from .utils import KContactSortedArray, ThreadManager, try_function
 
 
 class KademliaService(ProtocolService):
-    def __init__(self, id:int, k: int, b:int, a:int):
-        super(KademliaService, self).__init__(id, k, b)
+    def __init__(self, my_contact:Contact, k: int, b:int, a:int):
+        super(KademliaService, self).__init__(my_contact, k, b)
+        self.a = a
 
     def on_connect(self, conn:Connection):
         pass
@@ -17,7 +21,47 @@ class KademliaService(ProtocolService):
         pass
 
     def exposed_client_find_node(self, id:int) -> list:
-        pass
+        queue = Queue()
+        visited = set()
+        top_contacts = KContactSortedArray(self.k, id)
+        queue_lock = Semaphore()
+        for contact in self.table.get_closest_buckets(id):
+            queue.put(contact)
+            visited.add(contact)
+            top_contacts.push(contact)
+            if queue.qsize() >= self.a:
+                break
+        manager = ThreadManager(self.a, queue.qsize, self.find_node_lookup, args=(queue, top_contacts, visited, queue_lock))
+        manager.start()
+        for contact in top_contacts:
+            if contact.hash == id:
+                return contact
+        return None
+
+    def find_node_lookup(self, queue:Queue, top:KContactSortedArray, visited:set, queue_lock:Semaphore):
+        contact = None
+        try:
+            contact = queue.get(timeout=1)
+        except Empty:
+            return
+        result, connection = self.ping(contact.ip, contact.port)
+        if not result:
+            return
+        self.table.update(contact)
+        bucket = connection.root.find_node(self.my_contact, contact.hash)
+        for new_contact in bucket:
+            result, _ = self.ping(new_contact.ip, new_contact.port)
+            if not result:
+                continue
+            self.table.update(new_contact)
+            queue_lock.acquire()
+            if not new_contact in visited:
+                visited.add(new_contact)
+                queue_lock.release()
+                queue.put(new_contact)
+                top.push(new_contact)
+            else:
+                queue_lock.release()
 
     def exposed_client_find_value(self, key:int) -> object:
         pass
