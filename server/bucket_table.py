b35adabc86d09cf5d1828cca3204eb156ca11fcd
stdevCbermudez <cbermu.97@gmail.com>
2019-11-12 11:07:38 -0500

    Add logs to BucketTable class

--- server/bucket_table.py
+++ server/bucket_table.py
@@ -13,21 +13,29 @@ class BucketTable:
             self.buckets.append(Bucket(self.k))
 
     def get_bucket(self, id:int) -> Bucket:
+        debug(f'BucketTable.get_bucket - Getting bucket for id: {id}.')
         index = self.get_bucket_index(id)
         self.buckets[index].semaphore.acquire()
         result = self.buckets[index]
+        debug(f'BucketTable.get_bucket - Result bucket with index: {index} is result: {result}.')
         self.buckets[index].semaphore.release()
         return result
 
     def get_bucket_index(self, id:int) -> int:
+        debug(f'BucketTable.get_bucket_index - Getting bucket index for id: {id}.')
         distance = self.id ^ id
-        debug(f'Distance between {self.id} and {id} = {distance}')
-        return max([i for i in range(self.b) if distance & (1<<i) > 0], default=0)
+        debug(f'BucketTable.get_bucket_index - Distance between {self.id} and {id} = {distance}.')
+        index = max([i for i in range(self.b) if distance & (1<<i) > 0], default=0)
+        debug(f'BucketTable.get_bucket_index - Resulting index: {index}.')
+        return index
 
     def update(self, contact:Contact) -> bool:
+        debug(f'BucketTable.update - Updating contact: {contact}.')
         bucket = self.get_bucket(contact.id)
+        debug(f'BucketTable.update - Bucket to update bucket: {bucket}.')
         bucket.semaphore.acquire()
         result = bucket.update(contact)
+        debug(f'BucketTable.update - Result for update result: {result}.')
         bucket.semaphore.release()
         return result
 
@@ -38,33 +46,44 @@ class BucketTable:
         left = self.buckets[:index]
         center = self.buckets[index]
         right = self.buckets[index+1:]
+        debug(f'BucketTable.get_closest_buckets - Iterators members are left: {left} center:{center} right: {right}.')
         lindex = len(left) - 1
         rindex = 0
         center.semaphore.acquire()
+        debug(f'BucketTable.get_closest_buckets - Begin iteration over center.')
         for contact in center:
             yield contact
+        debug(f'BucketTable.get_closest_buckets - End iteration over center.')
         center.semaphore.release()
         while lindex >= 0 and rindex < len(right):
             left[lindex].semaphore.acquire()
+            debug(f'BucketTable.get_closest_buckets - Begin iteration over one member of left.')
             for contact in left[lindex]:
                 yield contact
+            debug(f'BucketTable.get_closest_buckets - End iteration over one member of left.')
             left[lindex].semaphore.release()
             right[rindex].semaphore.acquire()
+            debug(f'BucketTable.get_closest_buckets - Begin iteration over one member of right.')
             for contact in right[rindex]:
                 yield contact
+            debug(f'BucketTable.get_closest_buckets - End iteration over one member of right.')
             right[rindex].semaphore.release()
             lindex -= 1
             rindex += 1
         while lindex >= 0:
             left[lindex].semaphore.acquire()
+            debug(f'BucketTable.get_closest_buckets - Begin iteration over one member of left.')
             for contact in left[lindex]:
                 yield contact
+            debug(f'BucketTable.get_closest_buckets - End iteration over one member of left.')
             left[lindex].semaphore.release()
             lindex -= 1
         while rindex < len(right):
             right[rindex].semaphore.acquire()
+            debug(f'BucketTable.get_closest_buckets - Begin iteration over one member of right.')
             for contact in right[rindex]:
                 yield contact
+            debug(f'BucketTable.get_closest_buckets - End iteration over one member of right.')
             right[rindex].semaphore.release()
             rindex += 1
         debug(f'BucketTable.get_closest_buckets - Finish the method')
