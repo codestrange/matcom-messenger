e97e2e225c809de75b87aefe9c0e20a1ea01cd04
leynier <leynier41@gmail.com>
2019-12-01 17:54:36 -0500

    Fix README.md

---
+++ test.py
@@ -0,0 +1,49 @@
+import rpyc
+from server import *
+
+leynier = None
+
+
+def test1():
+    nodes = rpyc.discover('TRACKER')
+
+    assert len(nodes) == 2
+
+    node_c = nodes[0] if nodes[0][0].split('.')[3] == '13' else nodes[1]
+    node_l = nodes[1] if nodes[0][0].split('.')[3] == '13' else nodes[0]
+
+    id_c = get_id(node_c)
+    id_l = get_id(node_l)
+
+    c_c = rpyc.connect(*node_c)
+    c_l = rpyc.connect(*node_l)
+
+    print(c_c.root.client_store(leynier.get_id(), leynier.to_json()))
+    c_c.root.client_data()
+    c_c.root.client_table()
+    c_l.root.client_data()
+    c_l.root.client_table()
+
+
+def test2():
+    conn = rpyc.connect('localhost', 8081)
+    _, name_time = leynier.get_name()
+    leynier.set_name('carlos', name_time + 1)
+    print(conn.root.client_store(leynier.get_id(), leynier.to_json()))
+    conn.root.client_data()
+
+
+def test3():
+    conn = rpyc.connect('192.168.43.13', 8081)
+    print(conn.root.client_find_value(leynier.get_id()))
+    conn.root.client_data()
+
+
+if __name__ == "__main__":
+    print('Running test ...')
+    leynier = UserData('leynier', '+5353478301', '1234')
+    tests = [test1, test2, test3]
+    for test in tests:
+        input(f'Press key for run the test: {test.__name__} ')
+        print(f'Result: ', end='')
+        test()
