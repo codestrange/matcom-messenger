0084981c177b95d7aa4365cd79a01369e8bc2dee
leynier <leynier41@gmail.com>
2019-12-05 16:56:01 -0500

    Add flask admin

--- client/app/__init__.py
+++ client/app/__init__.py
@@ -1,10 +1,14 @@
 from threading import Thread
 from flask import Flask
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from flask_cors import CORS
 from rpyc.utils.server import ThreadedServer
 from .config import config
-from .models import db
+from .models import db, ContactModel, MessageModel, UserModel
 from ..service import ClientService
+
+admin = Admin()
 
 
 def create_app(config_name):
@@ -13,11 +17,16 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
+    admin.init_app(app)
     db.init_app(app)
     app.db = db
 
     thread = Thread(target=start_service, args=(app, ))
     thread.start()
+
+    admin.add_view(ModelView(ContactModel, db.session))
+    admin.add_view(ModelView(MessageModel, db.session))
+    admin.add_view(ModelView(UserModel, db.session))
 
     from .controllers.main import main_blueprint
     app.register_blueprint(main_blueprint)
