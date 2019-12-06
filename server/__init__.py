ec8935e37b085baeef43f14c83fa6b10beb03126
leynier <leynier41@gmail.com>
2019-12-05 20:58:35 -0500

    Import Message class in server

--- server/__init__.py
+++ server/__init__.py
@@ -2,6 +2,7 @@ from .bucket_table import BucketTable
 from .bucket import Bucket
 from .contact import Contact
 from .kademlia import KademliaService
+from .message import Message
 from .tracker import TrackerService
 from .user_data import UserData
 from .utils import connect, KContactSortedArray, ThreadManager, get_hash, get_id, try_function
