6f605792d7b04207993015b991c78f0b48170a66
cbermudez <cbermu.97@gmail.com>
2019-12-08 21:19:34 -0500

    Make a project entrypoint

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
+from os import system
 from sys import argv
+from multiprocessing import Process
 from server import TrackerService
 
 
-TrackerService.start(len(argv) > 1)
+def start_tracker(random: bool):
+    TrackerService.start(random)
+
+def start_client():
+    system("""
+    export FLASK_APP=client/run.py &&
+    export FLASK_ENV=production    &&
+    export FLASK_DEBUG=0           &&
+    flask run
+    """)
+
+if __name__ == "__main__":
+    Process(target=start_tracker, args=('random' in argv[1:],)).start()
+    Process(target=start_client).start()
